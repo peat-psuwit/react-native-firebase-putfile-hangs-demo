9a1a8ecf1e7a2d85c2e1b04d64f0801d6cdab450
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-10-13 23:14:06 +0700

    Implement test file creation

--- App.js
+++ App.js
@@ -13,26 +13,55 @@ import {
   Platform,
   StyleSheet,
   Text,
+  ToastAndroid,
   View,
 } from 'react-native';
 
-type Props = {};
-export default class App extends Component<Props> {
-  handleCreatePressed = () => {
+import RNFS from 'react-native-fs';
 
+type Props = {};
+type State = {
+  working: boolean,
+};
+
+const TEST_FILE_PATH = `${RNFS.DocumentDirectoryPath}/test.txt`;
+
+export default class App extends Component<Props, State> {
+  state = {
+    working: false,
+  };
+
+  handleCreatePressed = async () => {
+    this.setWorking(true);
+
+    try {
+      await RNFS.writeFile(TEST_FILE_PATH, 'This is a test.', 'utf8');
+      ToastAndroid.show('Test file created.', ToastAndroid.SHORT);
+    } finally {
+      this.setWorking(false);
+    }
   }
 
   handleUploadPressed = () => {
 
   }
 
+  setWorking(working: boolean) {
+    this.setState({ working });
+  }
+
   render() {
+    const { working } = this.state;
+
     return (
       <View style={styles.container}>
         <Text style={styles.title}>RNFirebase PutFile URL bug</Text>
 
         <View style={styles.activityIndicatorView}>
-          <ActivityIndicator size={48} />
+          {working
+            ? <ActivityIndicator size={48} />
+            : null
+          }
         </View>
 
         <View style={styles.buttonsView}>
@@ -40,12 +69,14 @@ export default class App extends Component<Props> {
             <Button
               title="Create test file"
               onPress={this.handleCreatePressed}
+              disabled={working}
             />
           </View>
           <View style={styles.button}>
             <Button
               title="Upload test file"
               onPress={this.handleUploadPressed}
+              disabled={working}
             />
           </View>
         </View>
